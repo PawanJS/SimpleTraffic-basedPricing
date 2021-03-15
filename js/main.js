b49c07a40e15508402f4cf24bc5bbb05cca8a604
PawanJS <contactpawanjs@gmail.com>
2021-03-15 10:06:37 +0530

    added js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,43 @@
 "use strict";
 const slider = document.querySelector(".js-slider");
+const btn = document.querySelector(".js-btn");
 
+// Making slider work
 slider.oninput = function () {
   const pageViews = document.querySelector(".js-pageViews");
   const price = document.querySelector(".js-price");
+  const toggle = document.querySelector(".js-toggle");
+
+  function printingPrice(amount) {
+    if (toggle.checked) {
+      price.textContent = `$${amount - amount * 0.25}`;
+    } else {
+      price.textContent = `$${amount}`;
+    }
+  }
 
   if (Number(this.value) === 20) {
     pageViews.textContent = "10k";
-    price.textContent = "$8";
+    printingPrice(8);
   } else if (Number(this.value) === 40) {
     pageViews.textContent = "50k";
-    price.textContent = "$12";
+    printingPrice(12);
   } else if (Number(this.value) === 60) {
     pageViews.textContent = "100k";
-    price.textContent = "$16";
+    printingPrice(16);
   } else if (Number(this.value) === 80) {
     pageViews.textContent = "500k";
-    price.textContent = "$24";
+    printingPrice(24);
   } else if (Number(this.value) === 100) {
     pageViews.textContent = "1M";
-    price.textContent = "$36";
+    printingPrice(36);
   }
 };
+
+// Refreshing page on button click
+function refreshPage() {
+  window.location.reload();
+}
+
+// Event handlers
+btn.addEventListener("click", refreshPage);
