564baf07951a18c709d80b0821a954ea0b08e965
PawanJS <contactpawanjs@gmail.com>
2021-03-19 08:45:54 +0530

    Added toggle event

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@ const btn = document.querySelector(".js-btn");
 const toggle = document.querySelector(".js-toggle");
 
 // Making slider work
+function setBar(value) {
+  const fill = document.querySelector(".js-fill");
+  fill.style.width = `${value}%`;
+}
+
+setBar(50);
+
 slider.oninput = function () {
   const pageViews = document.querySelector(".js-pageViews");
   const price = document.querySelector(".js-price");
@@ -16,21 +23,30 @@ slider.oninput = function () {
     }
   }
 
+  function printPageViews(view) {
+    pageViews.textContent = `${view}k`;
+  }
+
   if (Number(this.value) === 20) {
-    pageViews.textContent = "10k";
+    printPageViews(10);
     printingPrice(8);
+    setBar(0);
   } else if (Number(this.value) === 40) {
-    pageViews.textContent = "50k";
+    printPageViews(50);
     printingPrice(12);
+    setBar(30);
   } else if (Number(this.value) === 60) {
-    pageViews.textContent = "100k";
+    printPageViews(100);
     printingPrice(16);
+    setBar(50);
   } else if (Number(this.value) === 80) {
-    pageViews.textContent = "500k";
+    printPageViews(500);
     printingPrice(24);
+    setBar(70);
   } else if (Number(this.value) === 100) {
     pageViews.textContent = "1M";
     printingPrice(36);
+    setBar(100);
   }
 };
 
